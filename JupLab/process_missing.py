be54e90724ea161ec788bfbf4b60749599c5d3d1
Al-Hitawi Mohammed <59222637+Mohammed20201991@users.noreply.github.com>
2023-03-06 00:15:19 +0100

    process v4 for hu lines data

--- JupLab/process_missing.py
+++ JupLab/process_missing.py
@@ -1,37 +1,46 @@
 import pandas as pd
 from pathlib import Path
 import zipfile
+import time
+import json
 Extract_files = False
+CLEAN_TEXT = True
 if Extract_files:
     with zipfile.ZipFile('/home/ngyongyossy/mohammad/OCR_HU_Tra2022/GPT-2_Parallel/process/en_words/images.zip','r') as zip:
         zip.extractall('/home/ngyongyossy/mohammad/OCR_HU_Tra2022/GPT-2_Parallel/process/en_words/images/')
 
-path = "/home/ngyongyossy/mohammad/OCR_HU_Tra2022/GPT-2_Parallel/process/sample/" 
-path2= "/home/ngyongyossy/mohammad/trdghm/TextRecognitionDataGeneratorHuMu23/trdg/out/"
-path4= "/home/ngyongyossy/mohammad/OCR_HU_Tra2022/GPT-2_Parallel/process/en_words/"
+ 
+path = "/home/ngyongyossy/mohammad/OCR_HU_Tra2022/GPT-2_Parallel/process/lines_hu_v4/"
 
-df = pd.read_csv(f'{path4}labels.txt',
+if CLEAN_TEXT:
+    import re
+    with open(f'{path}labels_tab_remove.txt', encoding='utf-8') as fp1:
+     with open(f'{path}labels_clean1.txt', mode='w', encoding='utf-8') as fp2:
+        for row in fp1:
+            if row.strip():
+                file_text, text = re.split(r'\s{3}', row, maxsplit=1)
+                text = re.sub(r'\s+', ' ', text)
+                fp2.write(f"{file_text}{' ':4}{text}\n")
+
+print('File has cleaned ::')
+time.sleep(5)
+
+df = pd.read_csv(f'{path}labels_clean1.txt', 
                  header=None,
                  delimiter='   ',
                  encoding="utf8",
                  error_bad_lines=False,
-                 engine='python'
-                 )
+                 engine='python',
+                 ) 
 df.rename(columns={0: "file_name", 1: "text"}, inplace=True)
 print(df.head())
-# print(df.tail())
-# print(len(df))
+print(len(df))
 
-
-def is_dir_exist(filename):
-    
-    path4= "/home/ngyongyossy/mohammad/OCR_HU_Tra2022/GPT-2_Parallel/process/en_words/"
+def is_dir_exist(filename): 
+    path = "/home/ngyongyossy/mohammad/OCR_HU_Tra2022/GPT-2_Parallel/process/lines_hu_v4/"
     # path_to_file = f'{path2}imgs/'+ filename # df['file_name'][idx] # 'readme.txt'
-    path_to_file = f'{path4}images/en/'+ filename # df['file_name'][idx] # 'readme.txt'
-
+    path_to_file = f'{path}images/'+ filename # df['file_name'][idx] # 'readme.txt'
     path = Path(path_to_file)
-
-    # print(path.is_file()) 
     return path.is_file() 
 
 def drop_row(idx):    
@@ -47,12 +56,12 @@ print('list of file names that exist in labels but not in imgs dir: \n', list_fn
 for i in list_fn:
     df.drop(df[df['file_name'] == i ].index, inplace = True)
 
-print("Data frame after processed" , df.head(11))
+time.sleep(3)
+print("Data frame after processed" , df.head(10))
 
 reddit = df.to_dict(orient= "records")
 print(type(reddit) , len(reddit))
 # we have list of dict[{},{},{}]
-import json 
-with open("train_en.jsonl","w") as f:
+with open(f"{path}train1.jsonl","w") as f:
     for line in reddit:
         f.write(json.dumps(line,ensure_ascii=False) + "\n")
