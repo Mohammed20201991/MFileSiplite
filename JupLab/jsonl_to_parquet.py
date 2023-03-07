a805590b7c2b05ac55e40147713a349cde3d8eae
Mohammed20201991 <mohamedabid092@gmail.com>
2023-03-07 09:06:15 +0100

    at this point I do small comparession on test SROIE size in diff foramt

--- JupLab/jsonl_to_parquet.py
+++ JupLab/jsonl_to_parquet.py
@@ -5,46 +5,48 @@ import numpy as np
 import pandas as pd
 import pyarrow as pa
 
-df = pd.DataFrame({'one': [-1, np.nan, 2.5],
-                   'two': ['foo', 'bar', 'baz'],
-                   'three': [True, False, True]},
-                   index=list('abc'))
-table = pa.Table.from_pandas(df)
+# df = pd.DataFrame({'one': [-1, np.nan, 2.5],
+#                    'two': ['foo', 'bar', 'baz'],
+#                    'three': [True, False, True]},
+#                    index=list('abc'))
+# table = pa.Table.from_pandas(df)
 
-pq.write_table(table, 'example.parquet')
+# pq.write_table(table, 'example.parquet')
 
-table2 = pq.read_table('example.parquet')
-print(table2)
+# table2 = pq.read_table('example.parquet')
+# print(table2)
 
-m = table2.to_pandas()
-print("m" ,m.head())
-print(table2.to_pandas())
+# m = table2.to_pandas()
+# print("m" ,m.head())
+# print(table2.to_pandas())
 
-n = pq.read_table('example.parquet', columns=['one', 'three'])
-print("n", type(n), n)
-x = pq.read_pandas('example.parquet', columns=['two']).to_pandas()
-print(x.head())
+# n = pq.read_table('example.parquet', columns=['one', 'three'])
+# print("n", type(n), n)
+# x = pq.read_pandas('example.parquet', columns=['two']).to_pandas()
+# print(x.head())
 
 
-pq_array = pa.parquet.read_table("example.parquet", memory_map=True)
-print("RSS: {}MB".format(pa.total_allocated_bytes() >> 20))
+# pq_array = pa.parquet.read_table("example.parquet", memory_map=True)
+# print("RSS: {}MB".format(pa.total_allocated_bytes() >> 20))
 
-pq_array = pa.parquet.read_table("example.parquet", memory_map=False)
-print("RSS: {}MB".format(pa.total_allocated_bytes() >> 20))
+# pq_array = pa.parquet.read_table("example.parquet", memory_map=False)
+# print("RSS: {}MB".format(pa.total_allocated_bytes() >> 20))
 
-# Inspecting the Parquet File Metadata
-parquet_file = pq.ParquetFile('example.parquet')
-print(parquet_file)
-metadata = parquet_file.metadata
-print(metadata)
+# # Inspecting the Parquet File Metadata
+# parquet_file = pq.ParquetFile('example.parquet')
+# print(parquet_file)
+# metadata = parquet_file.metadata
+# print(metadata)
 
 # ---------------------------------------------------
 # if Iam reading *.txt file 
 import pandas as pd
+input_file = '/home/ngyongyossy/mohammad/Data/SROIE_2019_text_recognition/test.jsonl'
+out_file = 'out.parquet'
 txt = False
 if txt:
     df = pd.read_csv(
-                      'input.txt' ,
+                      input_file,
                       header=None,
                       delimiter='   ',
                       encoding="utf8",
@@ -52,9 +54,9 @@ if txt:
                       )    
     df.rename(columns={0: "file_name", 1: "text"}, inplace=True)
 else: 
-    df = pd.read_json(path_or_buf = "input.jsonl", lines=True)
+    df = pd.read_json(path_or_buf = input_file, lines=True)
 
-df.to_parquet('out.parquet')
+df.to_parquet(out_file)
 # for vitulizing results 
 # https://www.parquet-viewer.com/
 # for extra reading 
@@ -65,10 +67,11 @@ parquet_file= r"/home/ngyongyossy/mohammad/OCR_HU_Tra2022/GPT-2_Parallel/out.par
 df =pd.read_parquet(parquet_file)
 print(df.head())
 
-# write without compression 
-csv_out = r"csv_out.csv"
-df.to_csv(csv_out,index=False,sep='\t')
+# # write without compression 
+# csv_out = r"csv_out.csv"
+# df.to_csv(csv_out,index=False,sep='\t')
 
-# write with gzip comparssion
-comparessed_csv= r"out.csv.gz"
-df.to_csv(comparessed_csv, index=False,compression='gzip')
+# # write with gzip comparssion
+# comparessed_csv= r"out.csv.gz"
+# df.to_csv(comparessed_csv, index=False,compression='gzip')
+print(df['file_name'][0], df['text'][0])
