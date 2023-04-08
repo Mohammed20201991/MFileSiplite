d56805888b13421bbf64772bddeee32a571d9de0
Al-Hitawi Mohammed <59222637+Mohammed20201991@users.noreply.github.com>
2023-04-08 13:01:59 +0200

    Cpoy hu names to hu_lines_v2

--- JupLab/CopyPast.py
+++ JupLab/CopyPast.py
@@ -1,6 +1,6 @@
 import shutil
 import os
-
+import time 
 # for moving single file 
 # capture source file 
 # original = r'/home/ngyongyossy/mohammad/trdghm/TextRecognitionDataGeneratorHuMu23/trdg/out/hu/lines/3/labels.txt'
@@ -14,21 +14,27 @@ import os
 # shutil.move(source, destination, copy_function = copy2)
 # Move All Files From A Directory
 
-# source_folder = r"/home/ngyongyossy/mohammad/trdghm/TextRecognitionDataGeneratorHuMu23/trdg/out/hu/lines/3/"
-source_folder = r"/home/ngyongyossy/mohammad/trdghm/TextRecognitionDataGeneratorHuMu23/trdg/out/lines/hu/"
-# destination_folder = r"/home/ngyongyossy/mohammad/OCR_HU_Tra2022/GPT-2_Parallel/process/lines_hu_v3/images/"
-destination_folder = r"/home/ngyongyossy/mohammad/OCR_HU_Tra2022/GPT-2_Parallel/process/lines_hu_v1/images/"
-
+source_folder = r"/home/ngyongyossy/mohammad/OCR_HU_Tra2022/GPT-2_Parallel/process/HuNames/images/"
+destination_folder = r"/home/ngyongyossy/mohammad/OCR_HU_Tra2022/GPT-2_Parallel/process/lines_hu_v2/images/"
 # print(os.listdir(source_folder))
+
+print('The data moving has started :')
+print(f'We will Copy {len(os.listdir(source_folder))} fiels: from {source_folder}   dir')
+print(f'We Copy {len(os.listdir(source_folder))}fiels:')
+time.sleep(3)
 # fetch all files
+# i = 0
 for file_name in os.listdir(source_folder):
     # construct full file path
     source = source_folder + file_name
-    destination = destination_folder + file_name
-    # move only files
+    destination = destination_folder + file_name  # for give uinqe names  str(i) +
+    # move only files (cut)
     if os.path.isfile(source):
-        shutil.move(source, destination)
+        # shutil.move(source, destination) # ctrl + X
+        shutil.copyfile(source, destination) # CTRL + C
         # print('Moved:', file_name)
-
+    # i = i+1
 # Move Files Matching a Pattern (Wildcard)
 # glob.glob(pathname, *, recursive=False)
+time.sleep(5)
+print(f'number of images after Copied {len(os.listdir(destination_folder))}  fiels: to {source_folder}   dir')
