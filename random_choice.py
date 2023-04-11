a7de25ab5cd80b74f1593b9e995411ef420874ff
Al-Hitawi Mohammed <59222637+Mohammed20201991@users.noreply.github.com>
2023-04-11 03:41:05 +0200

    randomly chosen 4000 sample iam

--- random_choice.py
+++ random_choice.py
@@ -4,12 +4,8 @@ import shutil
 import time 
 import json
 import os
-# file_path_type = ["./Memes/*.png", "./Memes/*.jpeg"]
-# ['/home/ngyongyossy/mohammad/OCR_HU_Tra2022/GPT-2_Parallel/process/test/*.jpg']
-# images = glob.glob(random.choice(file_path_type))
-# random_image = random.choice(images)
-source_folder      = '/home/ngyongyossy/mohammad/OCR_HU_Tra2022/GPT-2_Parallel/process/lines_hu_v3/'
-destination_folder = "/home/ngyongyossy/mohammad/OCR_HU_Tra2022/GPT-2_Parallel/process/lines_hu_v2/v3/"
+source_folder      = '/home/ngyongyossy/mohammad/Data/IAM_lines/'
+destination_folder = "/home/ngyongyossy/mohammad/OCR_HU_Tra2022/GPT-2_Parallel/process/lines_hu_v2/iam/"
 
 def load_jsonl(path):
     return pd.read_json(
@@ -19,7 +15,7 @@ df = load_jsonl(f'{source_folder}train.jsonl')
 print(df.head())
  
 # n is the number of samples we wanna choose  
-new_df = df.sample(n = 50000) #100 000
+new_df = df.sample(n = 4000) 
 print(len(new_df),new_df.head(3))
 #---------------------------------------------------
 # Converting new dataframe to jsonl
@@ -27,7 +23,7 @@ time.sleep(3)
 reddit = new_df.to_dict(orient= "records")
 print(type(reddit) , len(reddit))
 # we have list of dict[{},{},{}]
-with open(f"{destination_folder}100_000_sample_v3.jsonl","w") as f:
+with open(f"{destination_folder}4000_sample_iam.jsonl","w") as f:
     for line in reddit:
         f.write(json.dumps(line,ensure_ascii=False) + "\n")
 
@@ -40,16 +36,13 @@ def make_copy(file_name):
         shutil.copyfile(source, destination) # CTRL + C
         print('Copied :', file_name)
 
-
 # def load_jsonl2():
 #     return pd.read_json(
 #                         path_or_buf = f'{destination_folder}sample_v1.jsonl',
 #                         lines=True) 
 
-new_df = load_jsonl(f'{destination_folder}100_000_sample_v3.jsonl')
+new_df = load_jsonl(f'{destination_folder}4000_sample_iam.jsonl')
 print(new_df.head(10))
-# print(new_df['file_name'].iloc[:-1])
-# print(new_df.index[-1])
 
 # idx = new_df.index[-1]
 # print(len(new_df))
@@ -58,9 +51,3 @@ for idx in range(len(new_df)):
     file_name = new_df['file_name'][idx]  # come from df 
     print(file_name)
     make_copy(file_name)
-
-# for moving single file 
-# capture source file 
-# original = r'df['file_name][idx]'
-# capture destnation file 
-# target = r'/home/ngyongyossy/mohammad/OCR_HU_Tra2022/GPT-2_Parallel/process/lines_hu_v3/labels.txt'
