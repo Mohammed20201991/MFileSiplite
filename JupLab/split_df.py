e024777b045303d2df959db8e80167494f8eaccb
Al-Hitawi Mohammed <59222637+Mohammed20201991@users.noreply.github.com>
2023-04-15 03:23:34 +0200

    split Arrany200 train test
    the test set ratio = `0.05`

--- JupLab/split_df.py
+++ JupLab/split_df.py
@@ -1,19 +1,24 @@
+# Importing required libraries
+# import pandas as pd
+# df = pd.DataFrame(record)
+# test_df  = df.sample(frac = 0.5)
+# train_df = df.drop(test_df.index)
 import pandas as pd
 import time 
 import json
 
-source_folder      = '/home/ngyongyossy/mohammad/OCR_HU_Tra2022/GPT-2_Parallel/process/test/'
-destination_folder = "/home/ngyongyossy/mohammad/OCR_HU_Tra2022/GPT-2_Parallel/process/test/"
+source_folder      = "/home/ngyongyossy/mohammad/Data/DH-Lab/"
+destination_folder = "/home/ngyongyossy/mohammad/Data/DH-Lab/"
 
 def load_jsonl(path):
     return pd.read_json(
                         path_or_buf = path,
                         lines=True) 
-df = load_jsonl(f'{source_folder}Mix.jsonl')
+df = load_jsonl(f'{source_folder}train.jsonl')
 print(df.head())
  
 # n is the number of samples we wanna choose  
-test_df = df.sample(frac= 0.1) 
+test_df = df.sample(frac= 0.05) 
 print(len(test_df),test_df.head(3))
 #---------------------------------------------------
 # Converting new dataframe to jsonl
@@ -25,12 +30,11 @@ with open(f"{destination_folder}test.jsonl","w") as f:
     for line in reddit:
         f.write(json.dumps(line,ensure_ascii=False) + "\n")
 # /--------------------------------------------------------
-
 train_df = df.drop(test_df.index) 
 time.sleep(3)
 reddit = train_df.to_dict(orient= "records")
 print(type(reddit) , len(reddit))
 # we have list of dict[{},{},{}]
-with open(f"{destination_folder}train.jsonl","w") as f:
+with open(f"{destination_folder}new_train.jsonl","w") as f:
     for line in reddit:
         f.write(json.dumps(line,ensure_ascii=False) + "\n")
