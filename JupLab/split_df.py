8e4c7de8caaecd0b4d4bb7c4b1aaba528df274f2
Al-Hitawi Mohammed <59222637+Mohammed20201991@users.noreply.github.com>
2023-04-19 17:18:26 +0200

    Sipliting HuWords_dic
    At this phase, we are splitting the Hungarian words level dataset to train and test : 
    ```
    venv) ngyongyossy@a100:~/mohammad/OCR_HU_Tra2022/GPT-2_Parallel/process$ python3 split_df.py 
              file_name text
    0  words_dict_0.jpg   és
    1  words_dict_1.jpg   az
    2  words_dict_2.jpg   Az
    3  words_dict_3.jpg   is
    4  words_dict_4.jpg  egy
    3017                   file_name          text
    56236  words_dict_56249.jpg         Kenzó
    31607  words_dict_31614.jpg         Malta
    20721  words_dict_20727.jpg  kísérletileg
    <class 'list'> 3017
    <class 'list'> 57315
    ```

--- JupLab/split_df.py
+++ JupLab/split_df.py
@@ -7,14 +7,14 @@ import pandas as pd
 import time 
 import json
 
-source_folder      = "/home/ngyongyossy/mohammad/Data/DH-Lab/"
-destination_folder = "/home/ngyongyossy/mohammad/Data/DH-Lab/"
+source_folder      = '/home/ngyongyossy/mohammad/OCR_HU_Tra2022/GPT-2_Parallel/process/hu_words_v1/'
+destination_folder = "/home/ngyongyossy/mohammad/OCR_HU_Tra2022/GPT-2_Parallel/process/hu_words_v1/"
 
 def load_jsonl(path):
     return pd.read_json(
                         path_or_buf = path,
                         lines=True) 
-df = load_jsonl(f'{source_folder}train.jsonl')
+df = load_jsonl(f'{source_folder}hu_words_train.jsonl')
 print(df.head())
  
 # n is the number of samples we wanna choose  
@@ -30,11 +30,12 @@ with open(f"{destination_folder}test.jsonl","w") as f:
     for line in reddit:
         f.write(json.dumps(line,ensure_ascii=False) + "\n")
 # /--------------------------------------------------------
+
 train_df = df.drop(test_df.index) 
 time.sleep(3)
 reddit = train_df.to_dict(orient= "records")
 print(type(reddit) , len(reddit))
 # we have list of dict[{},{},{}]
-with open(f"{destination_folder}new_train.jsonl","w") as f:
+with open(f"{destination_folder}hu_words_train.jsonl","w") as f:
     for line in reddit:
         f.write(json.dumps(line,ensure_ascii=False) + "\n")
